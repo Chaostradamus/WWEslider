08a9c502a19f19cfa883041a86ec0c05992066d1
Chaostradamus <71539742+Chaostradamus@users.noreply.github.com>
2021-07-15 20:12:58 -0400

    Slider

---
+++ src/components/SliderData.js
@@ -0,0 +1,25 @@
+export const SliderData = [
+  {
+    image:
+      "https://www.wwe.com/f/styles/og_image/public/rd-talent/Bio/Ultimate_Warrior_bio.jpg",
+  },
+  {
+    image:
+      "https://bloximages.newyork1.vip.townnews.com/postandcourier.com/content/tncms/assets/v3/editorial/c/92/c92d09ee-ae7f-11eb-b032-4706af4d89c2/60940b543406e.image.jpg?resize=1200%2C1200",
+  },
+  {
+    image: "https://s33236.pcdn.co/wp-content/uploads/2012/04/mr-perfect.jpg",
+  },
+  {
+    image:
+      "https://images.squarespace-cdn.com/content/v1/5c6b2413b10f25e54df84726/1621302440807-G9GQ62R5FEN1PQYURU2H/Screenshot+2021-05-17+at+9.41.49+PM.png?format=1000w",
+  },
+  {
+    image:
+      "https://img.bleacherreport.net/img/images/photos/003/889/004/051c721091b0b12b77f1b07ba2028919_crop_exact.jpg?w=1200&h=1200&q=75",
+  },
+  {
+    image:
+      "https://www.ewrestlingnews.com/wp-content/uploads/2021/07/Roman-Reigns.jpg",
+  },
+];
